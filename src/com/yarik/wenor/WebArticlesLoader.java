41111c122f72e724feafaf61b83542014c79c50d
smile_p1 <smile_p1@smile-ThinkPad-Edge.(none)>
2015-09-01 15:54:30 +0300

    fix webarticles loader

--- src/com/yarik/wenor/WebArticlesLoader.java
+++ src/com/yarik/wenor/WebArticlesLoader.java
@@ -1,8 +1,6 @@
 package com.yarik.wenor;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -15,15 +13,17 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.X509TrustManager;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HTTP;
+import org.apache.http.impl.conn.SingleClientConnManager;
 import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -35,102 +35,96 @@ import android.util.Log;
 
 public class WebArticlesLoader extends AsyncTaskLoader<List<Story>> {
 
-	private Context context;
-	private List<Story> articlesList;
+    private final Context context;
+    private List<Story> articlesList;
 
-	private static final String URL = "https://nextto.com/homework.txt?";
+    private static final String URL = "https://nextto.com/homework.txt?";
 
-	public WebArticlesLoader(Context context) {
-		super(context);
-		this.context = context;
-	}
-	
-	private void trustEveryone() {
-		try {
-			HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier(){
-	    			
-					@Override
-					public boolean verify(String hostname, SSLSession session) {
-						
-						return true;
-					}});
-			SSLContext context = SSLContext.getInstance("TLS");
-			context.init(null, new X509TrustManager[]{new X509TrustManager(){
-				public void checkClientTrusted(X509Certificate[] chain,
-						String authType) throws CertificateException {}
-				public void checkServerTrusted(X509Certificate[] chain,
-						String authType) throws CertificateException {}
-				public X509Certificate[] getAcceptedIssuers() {
-					return new X509Certificate[0];
-				}}}, new SecureRandom());
-			HttpsURLConnection.setDefaultSSLSocketFactory(
-					context.getSocketFactory());
-		} catch (Exception e) { // should never happen
-			e.printStackTrace();
-		}
-	}
+    public WebArticlesLoader(final Context context) {
+        super(context);
+        this.context = context;
+    }
 
-	@Override
-	public List<Story> loadInBackground() {
-		/*
-		 * BufferedReader reader = null; StringBuffer lines = new
-		 * StringBuffer(); try { reader = new BufferedReader(new
-		 * InputStreamReader(context .getAssets().open("articles2.txt"),
-		 * "UTF-8")); String line; while ((line = reader.readLine()) != null) {
-		 * lines.append(line); } } catch (IOException e) { Log.e("readArticles",
-		 * e.getMessage()); } finally { if (reader != null) { try {
-		 * reader.close(); } catch (IOException e) { Log.e("readArticles",
-		 * e.getMessage()); } } } JSONArray articlesJSONArray = (JSONArray)
-		 * JSONValue.parse(lines .toString()); if (articlesList == null) {
-		 * articlesList = new ArrayList<>(); } for (int i = 0; i <
-		 * articlesJSONArray.size(); i++) { JSONObject article = (JSONObject)
-		 * articlesJSONArray.get(i); Story story = new Story(article);
-		 * articlesList.add(story); }
-		 * 
-		 * return articlesList;
-		 */
+    private void trustEveryone() {
+        try {
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier(){
 
-		trustEveryone();
-		HttpUriRequest request = new HttpGet(URL.toString());
-		request.addHeader(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                @Override
+                public boolean verify(final String hostname, final SSLSession session) {
 
-		HttpClient client = new DefaultHttpClient();
-		HttpResponse response = null;
-		String responseString = null;
-		try {
-			response = client.execute(request);
-			HttpEntity entity = response.getEntity();
-			responseString = EntityUtils.toString(entity, "UTF-8");
-		} catch (ClientProtocolException e) {
-			Log.e("protocol exc", e.getMessage());
-		} catch (IOException e) {
-			Log.e("io exc", e.getMessage());
-		}
+                    return true;
+                }});
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new X509TrustManager[]{new X509TrustManager(){
+                @Override
+                public void checkClientTrusted(final X509Certificate[] chain,
+                    final String authType) throws CertificateException {}
+                @Override
+                public void checkServerTrusted(final X509Certificate[] chain,
+                    final String authType) throws CertificateException {}
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }}}, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(
+                context.getSocketFactory());
+        } catch (Exception e) { // should never happen
+            e.printStackTrace();
+        }
+    }
 
-		if (responseString != null) {
-			JSONArray articlesJSONArray = (JSONArray) JSONValue.parse(responseString);
-			if (articlesList == null) {
-				articlesList = new ArrayList<>();
-			}
-			for (int i = 0; i < articlesJSONArray.size(); i++) {
-				JSONObject article = (JSONObject) articlesJSONArray.get(i);
-				Story story = new Story(article);
-				articlesList.add(story);
-			}
-		}
+    @Override
+    public List<Story> loadInBackground() {
+        HttpClient defClient = new DefaultHttpClient();
+        HostnameVerifier hostnameVerifier = org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+        SchemeRegistry registry = new SchemeRegistry();
+        SSLSocketFactory socketFactory = SSLSocketFactory.getSocketFactory();
+        socketFactory.setHostnameVerifier((X509HostnameVerifier) hostnameVerifier);
+        registry.register(new Scheme("https", socketFactory, 443));
+        HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier);
+        SingleClientConnManager singleClientConnManager = new SingleClientConnManager(defClient.getParams(), registry);
+        DefaultHttpClient httpclient = new DefaultHttpClient(singleClientConnManager, defClient.getParams());
 
-		return articlesList;
-	}
+        HttpResponse response = null;
+        try {
+            HttpGet httpGet = new HttpGet(WebArticlesLoader.URL);
+            response = httpclient.execute(httpGet);
+        } catch (ClientProtocolException e) {
+            Log.e("protocol exc", e.getMessage());
+        } catch (IOException e) {
+            Log.e("io exc", e.getMessage());
+        }
 
-	@Override
-	protected void onStartLoading() {
-		if (articlesList != null) {
-			deliverResult(articlesList);
-		}
+        String json = null;
+        try {
+            json = EntityUtils.toString(response.getEntity());
+        } catch (ParseException e) {
+            Log.e("parse exc", e.getMessage());
+        } catch (IOException e) {
+            Log.e("io exc", e.getMessage());
+        }
+        JSONArray articlesJSONArray = (JSONArray) JSONValue.parse(json);
 
-		if (takeContentChanged() || articlesList == null) {
-			forceLoad();
-		}
-	}
+        if (this.articlesList == null) {
+            this.articlesList = new ArrayList<>();
+        }
+        for (int i = 0; i < articlesJSONArray.size(); i++) {
+            JSONObject article = (JSONObject) articlesJSONArray.get(i);
+            this.articlesList.add(new Story(article));
+        }
+
+        return this.articlesList;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        if (this.articlesList != null) {
+            deliverResult(this.articlesList);
+        }
+
+        if (takeContentChanged() || (this.articlesList == null)) {
+            forceLoad();
+        }
+    }
 
 }
